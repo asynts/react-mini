697e3ae20efb478717a6e4d36211a32a19ba9453
Paul Scharnofske <asynts@gmail.com>
2022-11-18 17:26:06 +0100

    Port remaining components to JSX

--- src/index.jsx
+++ src/index.jsx
@@ -1,86 +1,17 @@
 import {
     Instance,
-    TextNode,
-    HtmlNode,
-    ComponentNode,
 } from "./react-mini.js";
 
 function IncrementComponent({ properties, useState }) {
     let [counter, setCounter] = useState("counter", 0);
 
-    /*
-    <div #1 key="root" class="component">
-        <h3 #1.1 key="0">
-            <text #1.2 key="1">Increment Component</text>
-        </h3>
-        <p #2 key="1">
-            <text #3 key="1">{counter}</text>
-        </p>
-        <button #4 key="2" onClick={() => setCounter(counter + 1)}>
-            <text #5 key="1">Increment</text>
-        </button>
-    </div>
-    */
-    let node_5 = new TextNode({
-        text: "Increment",
-        properties: {
-            key: "1",
-        }
-    });
-    let node_4 = new HtmlNode({
-        elementType: "button",
-        properties: {
-            key: "2",
-            $click: () => setCounter(counter + 1),
-        },
-        children: [
-            node_5,
-        ],
-    });
-    let node_3 = new TextNode({
-        text: `${counter}`,
-        properties: {
-            key: "1",
-        }
-    });
-    let node_2 = new HtmlNode({
-        elementType: "p",
-        properties: {
-            key: "1",
-        },
-        children: [
-            node_3,
-        ],
-    });
-    let node_1_2 = new TextNode({
-        text: "Increment Component",
-        properties: {
-            key: "1",
-        },
-    });
-    let node_1_1 = new HtmlNode({
-        elementType: "h3",
-        properties: {
-            key: "0",
-        },
-        children: [
-            node_1_2,
-        ],
-    });
-    let node_1 = new HtmlNode({
-        elementType: "div",
-        properties: {
-            key: "root",
-            class: "component",
-        },
-        children: [
-            node_1_1,
-            node_2,
-            node_4,
-        ],
-    });
-
-    return node_1;
+    return (
+        <div key={properties.key} class="component">
+            <h3 key="1">Increment Component</h3>
+            <p key="2">{counter.toString()}</p>
+            <button key="3" $click={() => setCounter(counter + 1)}>Increment</button>
+        </div>
+    );
 }
 
 function CalculatorComponent({ properties, useState }) {
@@ -98,83 +29,14 @@ function CalculatorComponent({ properties, useState }) {
         };
     }
 
-    /*
-    <div #1 key="root" class="component">
-        <h3 #1.1 key="0">
-            <text #1.2 key="1">Calculator Component</text>
-        </h3>
-        <input #2 key="1" type="text" value={inputState.a} $change={onChange("a")} />
-        <input #3 key="2" type="text" value={inputState.b} $change={onChange("b")} />
-        <p #4 key="3">
-            <text #5 key="1">{parseInt(inputState.a) + parseInt(inputState.b)}</text>
-        </p>
-    </div>
-    */
-    let node_5 = new TextNode({
-        text: (parseInt(inputState.a) + parseInt(inputState.b)).toString(),
-        properties: {
-            key: "1",
-        },
-    });
-    let node_4 = new HtmlNode({
-        elementType: "p",
-        properties: {
-            key: "3",
-        },
-        children: [
-            node_5,
-        ],
-    });
-    let node_3 = new HtmlNode({
-        elementType: "input",
-        properties: {
-            key: "2",
-            type: "text",
-            value: inputState.b,
-            $input: onChange("b"),
-        },
-        children: [],
-    });
-    let node_2 = new HtmlNode({
-        elementType: "input",
-        properties: {
-            key: "1",
-            type: "text",
-            value: inputState.a,
-            $input: onChange("a"),
-        },
-        children: [],
-    });
-    let node_1_2 = new TextNode({
-        text: "Calculator Component",
-        properties: {
-            key: "1",
-        },
-    });
-    let node_1_1 = new HtmlNode({
-        elementType: "h3",
-        properties: {
-            key: "0",
-        },
-        children: [
-            node_1_2,
-        ],
-    });
-    let node_1 = new HtmlNode({
-        elementType: "div",
-        properties: {
-            key: "root",
-            class: "component",
-        },
-        children: [
-            node_1_1,
-            node_2,
-            node_3,
-            node_4,
-        ],
-    });
-
-    return node_1;
+    return (
+        <div key={properties.key} class="component">
+            <h3 key="1">Calculator Component</h3>
+            <input key="2" type="text" value={inputState.a} $input={onChange("a")} />
+            <input key="3" type="text" value={inputState.b} $input={onChange("b")} />
+            <p key="4">{(parseInt(inputState.a) + parseInt(inputState.b)).toString()}</p>
+        </div>
+    );
 }
 
 function ListComponent({ properties, useState }) {
@@ -198,8 +60,8 @@ function ListComponent({ properties, useState }) {
 
     return (
         <div key={properties.key} class="component">
-            <h3 key="0">List Component</h3>
-            <div key="1">
+            <h3 key="1">List Component</h3>
+            <div key="2">
                 {items.map((item, index) => (
                     <div key={item}>
                         {item}
@@ -207,7 +69,7 @@ function ListComponent({ properties, useState }) {
                     </div>
                 ))}
             </div>
-            <button key="2" $click={() => appendNewItem()}>Append New</button>
+            <button key="3" $click={() => appendNewItem()}>Append New</button>
         </div>
     );
 }
